178140907213fe5f9404de642af959000eb7820f
wardenlym <wardenlym@gmail.com>
2019-03-01 16:27:55 +0800

    add install kubectl

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -2,5 +2,4 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt-get install -y kubectl
