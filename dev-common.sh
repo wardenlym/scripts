804edb5b2f1d61299852cd93570f7fe742024122
wardenlym <wardenlym@gmail.com>
2019-04-10 11:55:27 +0800

    fix

---
+++ dev-common.sh
@@ -0,0 +1,5 @@
+sudo apt install zsh
+curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-docker-ce.sh | sh
+curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-golang.sh | sh
+curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-kubeadm.sh | sh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- install-golang.sh
+++ install-golang.sh
@@ -1,4 +1,5 @@
 VERSION="1.11.8.linux-amd64"
+rm go$VERSION.tar.gz
 wget https://dl.google.com/go/go$VERSION.tar.gz
 sudo tar -zxvf go$VERSION.tar.gz -C /usr/local/
 sudo ln -sf /usr/local/go/bin/go /usr/bin/go
