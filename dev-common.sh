769e1979d32368f7ad97f25ffb6f4a1c3e63221b
wardenlym <wardenlym@gmail.com>
2019-04-18 16:25:04 +0800

    fix

--- dev-common.sh
+++ dev-common.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y git python zsh tmux vim
+sudo apt-get install -y git python zsh tmux vim curl
 curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-docker-ce.sh | sh
 curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-golang.sh | sh
 curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-kubeadm.sh | sh
--- kvm.sh
+++ kvm.sh
@@ -1,9 +1,13 @@
 sudo apt-get update && sudo apt-get install -y uml-utilities qemu-kvm bridge-utils virtinst libvirt-daemon-system libvirt-clients libguestfs-tools
 wget https://cloud-images.ubuntu.com/releases/18.04/release/ubuntu-18.04-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
 
-sudo virt-customize -a ubuntu-18.04-server-cloudimg-amd64.img --root-password password:root
+sudo virt-customize -a bionic-server-cloudimg-amd64.img --root-password password:root
 
-sudo qemu-system-x86_64 -enable-kvm -hda ubuntu-18.04-server-cloudimg-amd64.img  -m 512 -net nic -net tap,ifname=tap0,script=no -curses
+sudo virt-customize -a bionic-server-cloudimg-amd64.img --uninstall cloud-init
 
+sudo qemu-system-x86_64 -enable-kvm -hda bionic-server-cloudimg-amd64.img  -m 2048 -smp 2,cores=2,threads=1 -net nic -net tap,ifname=tap0,script=no -curses
+
+dhclient ens3
 
 https://serverascode.com/2018/06/26/using-cloud-images.html
