0ad6199e760fd3b912a9f60cdd22b0c0f459fd16
Ubuntu <ubuntu@ip-172-31-44-241.ap-northeast-1.compute.internal>
2019-12-27 05:51:36 +0000

    add elixir

--- setup-devel-env.sh
+++ setup-devel-env.sh
@@ -1,11 +1,13 @@
 sudo apt-get update
 
-sudo apt-get install -y git python zsh tmux vim
+sudo apt-get install -y make git python zsh tmux vim
 
 git clone https://github.com/wardenlym/dotfiles .dotfiles && cd .dotfiles && ./setup.sh
 
 curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-docker-ce.sh | sh
-curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-kubectl.sh | sh
+curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-kubeadm.sh | sh
+curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-golang.sh | sh
+curl https://raw.githubusercontent.com/wardenlym/scripts/master/install-elixir.sh | sh
 
 sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i 's/plugins=(git)/plugins=(git golang docker kubectl aws)/' .zshrc
